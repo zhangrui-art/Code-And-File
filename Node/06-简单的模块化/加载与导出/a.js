2cfee6ebbfd2cfad102ec05a779b532179fecb3c
RZ <zhangrui2804@163.com>
2023-12-06 18:28:28 +0800

    udpatre

---
+++ Node/06-简单的模块化/加载与导出/a.js
@@ -0,0 +1,6 @@
+const b = require('./b');
+
+console.log('开始执行a.js')
+console.log(b.foo);
+console.log(b.add(1,2));
+console.log('执行完毕a.js');
---
+++ Node/06-简单的模块化/加载与导出/b.js
@@ -0,0 +1,8 @@
+let foo = "bbb"
+
+// exports 初始值是 {}
+exports.foo = "hello"
+exports.add = (x,y) => {   
+    return x + y
+}
+ 
