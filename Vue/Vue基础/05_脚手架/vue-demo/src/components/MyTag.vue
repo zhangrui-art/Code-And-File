0ba9507ae4479103c45720b80b0ae5ae9373c4e4
RZ <zhangrui2804@163.com>
2024-03-12 14:14:52 +0800

    完成插槽相关

---
+++ Vue/Vue基础/05_脚手架/vue-demo/src/components/MyTag.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="my-tag">
+    <input 
+        class="input"
+        type="text"
+        placeholder="输入标签"
+        v-if="isEdit"
+        :value="value"
+        v-focus
+        @blur="isEdit = false"
+        @keyup.enter="handleEnter"
+      />
+    <div 
+      class="text"
+      v-else
+      @dblclick="isEdit = true"
+    >{{ value }}</div>
+  </div>  
+</template>
+
+<script>
+export default {
+  props: {
+    value: String
+  },
+  data() {
+    return {
+      isEdit: false
+    }
+  },
+  methods: {
+    handleEnter(e) {
+      if(!e.target.value) {
+        alert('请输入标签')
+        return
+      }
+      this.$emit('input', e.target.value)
+      this.isEdit = false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .my-tag {
+    cursor: pointer;
+    .input {
+      appearance: none;
+      outline: none;
+      border: 1px solid #ccc;
+      width: 100px;
+      height: 40px;
+      box-sizing: border-box;
+      padding: 10px;
+      color: #666;
+      &::placeholder {
+        color: #666;
+      }
+    }
+  }
+</style>
---
+++ Vue/Vue基础/05_脚手架/vue-demo/src_21_插槽作用域/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <MyTable :data="list">
+      <template #default="obj">
+        <button @click="del(obj.curItem.id)">删除</button>
+      </template>
+    </MyTable>
+    <MyTable :data="list2">
+      <template #default="{ curItem }">
+        <button @click="show(curItem)">查看</button>
+      </template>
+    </MyTable>
+  </div>
+</template>
+
+<script>
+import MyTable from './components/MyTable.vue'
+export default {
+  data () {
+    return {
+      list: [
+        { id: 1, name: '张小花', age: 18 },
+        { id: 2, name: '孙大明', age: 19 },
+        { id: 3, name: '刘德忠', age: 17 },
+      ],
+      list2: [
+        { id: 1, name: '赵小云', age: 18 },
+        { id: 2, name: '刘蓓蓓', age: 19 },
+        { id: 3, name: '姜肖泰', age: 17 },
+      ]
+    }
+  },
+  components: {
+    MyTable
+  },
+  methods: {
+    del(id) {
+      this.list = this.list.filter((item) => item.id !== id)
+    },
+    show(item) {
+      console.log(item)
+    }
+  }
+}
+</script>
