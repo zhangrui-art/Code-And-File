9047dcfdaa43d1f752fceb84fa88bcf844328a88
RZ <zhangrui2804@163.com>
2024-04-10 18:27:20 +0800

    update

--- Vue3/vue3-big-event-admin/src/stores/user.js
+++ Vue3/vue3-big-event-admin/src/stores/user.js
@@ -1,17 +1,23 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useUserStore = defineStore('user', () => {
-  const token = ref('')
-  const setToken = (newToken) => {
-    token.value = newToken
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const token = ref('')
+    const setToken = (newToken) => {
+      token.value = newToken
+    }
+    const removeToken = () => {
+      token.value = ''
+    }
+    return {
+      token,
+      setToken,
+      removeToken
+    }
+  },
+  {
+    persist: true
   }
-  const removeToken = () => {
-    token.value = ''
-  }
-  return {
-    token,
-    setToken,
-    removeToken
-  }
-})
+)
